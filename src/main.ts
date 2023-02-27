7d6f1b8918a16828df7b3604a633fdd87011cb73
noahGms <noah@keshin.net>
2023-02-27 13:42:28 +0100

    install and setup class validator for validation

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { PrismaService } from './prisma.service';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const prismaService = app.get(PrismaService);
   await prismaService.enableShutdownHooks(app);
+
+  app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(3000);
 }

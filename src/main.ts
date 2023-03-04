546121b7feaace9700586319321ffd891d5084dc
noahGms <noah@keshin.net>
2023-03-04 14:59:48 +0100

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors();
+
   const prismaService = app.get(PrismaService);
   await prismaService.enableShutdownHooks(app);
 

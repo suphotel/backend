4b6690fc89a9c3cc42fd6830d1e8ba052ea31425
noahGms <noah@keshin.net>
2023-03-05 22:50:01 +0100

    update main.ts and create .env.example file

--- src/main.ts
+++ src/main.ts
@@ -20,6 +20,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
 
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
 }
 bootstrap();
